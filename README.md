0b8e6278c70201bdd43e4cc56e3e660580b42728
ffreemt <yucongo+fmt@gmail.com>
2020-09-07 14:48:13 +0800

    update reload/load

--- README.md
+++ README.md
@@ -13,27 +13,31 @@ hot plug and remove nonebot plugins
 To validate installation
 ```
 python -c "import nbplugins_watch; print(nbplugins_watch.__version__)"
-0.0.1
+0.0.6
 ```
 
 ### Usage
-Make a directory somewhere and place an empty \_\_init\_\_.py in it.
+Make a directory somewhere and place an empty \_\_init\_\_.py and a plugin file in it.
 
 Monitor the directory in your nonebot runner file, e.g. in  `my_nonebot.py`:
 ```python
 
 import nonebot
+from nbplugins_watch import nbplugins_watch
 
 nonebot.load_builtin_plugins()  # optional
+nonebot.load_builtin_plugins("mature-plugin-dir")
 
 plugin_dir_path = r"path_to_plugin_dir"  # absolute or relative path
-from nbplugins_watch import nbplugins_watch
+
 nbplugins_watch(plugin_dir_path)
+# turn debug on to see what's going on
+# nbplugins_watch(plugin_dir_path, debug=True)
 
 nonebot.run()
 
 ```
-Create a file, say fancy_plugin.py, in the directory above. Edit and test and/or remove the file fancy_plugin.py to your heart's content.
+Create a file, say fancy_plugin.py if not already there, in the directory above. Edit and test and/or remove the file fancy_plugin.py to your heart's content.
 
 Note: <s>if a plugin file contains syntax errors (as opposed to logic errors), you'll have to restart nonebot, in other words, nbplugins_watch will cease to work after an uncaught error.</s> nbplugins_watch can recover from syntax errors, in other words, even when a plugin file contains syntax errors (as opposed to logic errors), it just still works.
 
--- nbplugins_watch/nbplugins_watch.py
+++ nbplugins_watch/nbplugins_watch.py
@@ -41,7 +41,7 @@ def _watch_n_reload(
 
     if not watch_dir:
         watch_dir = CURDIR
-    logger.debug("starting watchgod in %s", watch_dir)
+    logger.debug("\n\tstarting watchgod in %s\n", watch_dir)
 
     if not Path(watch_dir).is_dir():
         logger.error(" %s is not a directory or does not exist, exiting...")
@@ -60,13 +60,20 @@ def _watch_n_reload(
     if not (_ / "__init__.py").exists():
         logger.error(" __init__.py not present in %s", _)
         logger.info(
-            "You need to place an __init__.py in the directory. Exiting... not watching the directory..."
+            "You need to place an __init__.py in the directory *%s*. Exiting... not watching the directory...", _
         )
         return None
 
     # append p_dir to sys.path if not already in sys.path
     if p_dir not in sys.path:
         sys.path.append(p_dir)
+
+    # pylint: disable=protected-access
+    logger.debug("\n\t>plugin.PluginManager._plugins: %s", plugin.PluginManager._plugins)
+
+    # nonebot.load_plugins(mdir)  # this does not work
+
+    logger.debug("\n\t>plugin.PluginManager._plugins: %s", plugin.PluginManager._plugins)
 
     # async for changes in awatch(watch_dir):
     for changes in watch(watch_dir):
@@ -95,8 +102,18 @@ def _watch_n_reload(
                     # module_path = f"{Path(CURDIR).stem}.{Path(_i).stem}"
                     # module_path = f"{Path(watch_dir)}.{Path(_i).stem}"
                     module_path = f"{m_dir}.{Path(_i).stem}"
-                    res = plugin.PluginManager.remove_plugin(module_path)
-                    logger.info("\n\t %s removed: %s", module_path, res)
+
+                    if module_path in plugin.PluginManager._plugins:
+                        res = plugin.PluginManager.remove_plugin(module_path)
+                        logger.info("\n\t %s removed: %s", module_path, res)
+
+                    _ = """
+                    try:
+                        del plugin.PluginManager._plugins[module_path]
+                    except Exception as exc:
+                        logger.error(f" del plugin.PluginManager._plugin[{module_path}]: %s", exc)
+                    # """
+
                 except Exception as exc:
                     logger.error("PluginManager.remove exc: %s", exc)
                 continue
@@ -114,8 +131,8 @@ def _watch_n_reload(
 
             try:
                 file_ = Path(_i)
-                # if file_.exists():
-                list_.append(file_.stem)
+                if file_.exists():
+                    list_.append(file_.stem)
             except Exception as exc:
                 logger.debug("file_ = Path(_i) exc: %s (expected)", exc)
 
@@ -124,7 +141,9 @@ def _watch_n_reload(
         for _ in list_:
             try:
                 # _ = str(Path(CURDIR) / _)
-                logger.debug(" #reloading %s", _)
+
+                logger.debug("\n\t>plugin.PluginManager._plugins: %s", plugin.PluginManager._plugins)
+                logger.debug(" >>> re/loading %s", _)
 
                 # _ = plugin.reload_plugin(f"plugins1.{_}")
 
@@ -132,13 +151,20 @@ def _watch_n_reload(
                 # module_path = f"{Path(watch_dir)}.{_}"
                 module_path = f"{m_dir}.{_}"
                 logger.debug(" module_path: %s", module_path)
-                res = plugin.reload_plugin(module_path)
 
-                logger.info(" # %s reloaded: [%s]", module_path, res)
+                res = plugin.reload_plugin(module_path)
+                # try load_plugin for newly added files
+                if not res:
+                    res = plugin.load_plugin(module_path)
+
+                logger.info(" #># %s reloaded: [%s]", module_path, res)
+
+                logger.debug("\n\t>plugin.PluginManager._plugins: %s", plugin.PluginManager._plugins)
+
             except Exception as exc:
                 logger.error(" #plugin.reaload exc: %s", exc)
 
-        # await asyncio.sleep(5)
+        # await asyncio.sleep(2)
         sleep(2)
 
     logger.debug(
@@ -151,7 +177,7 @@ def _watch_n_reload(
 
 
 # def watch_n_reload(watch_dir: str = ""):
-def nbplugins_watch(watch_dir: str = "", debug: bool = False):
+def nbplugins_watch(watch_dir: str = "", debug: bool = True):
     """ watch and reaload plugins in a directory.
 
     Default to the current directory
@@ -168,9 +194,11 @@ def nbplugins_watch(watch_dir: str = "", debug: bool = False):
         logger.debug("resetting _watch_n_reload.flag to True")
 
         # to trigger some change in the directory
+        _ = """
         init_py = Path(_watch_n_reload.watch_dir) / "__init__.py"
         if init_py.exists():
             init_py.touch()
+        # """
     except Exception as exc:
         logger.debug("_watch_n_reload first run exc: %s (expected)", exc)
 
